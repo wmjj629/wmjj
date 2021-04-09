9d4d46c689ecd506db6563f670b86d4373683119
wmjj629 <wmjj629@nate.com>
2021-04-09 17:18:05 +0900

    3번째 커밋

--- StdAfx.cpp
+++ StdAfx.cpp
@@ -1,3 +1,4 @@
+// 테스트 2번째 커밋
 // stdafx.cpp : source file that includes just the standard includes
 //	AutoUpdate.pch will be the pre-compiled header
 //	stdafx.obj will contain the pre-compiled type information
